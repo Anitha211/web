d191c6ec0f2ed3aed797dfe36ae33ddaabc0ffe8
Anitha <anitha.atmakuru@nybinfotech.com>
2026-02-02 09:08:34 +0530

    React files

--- React/client/src/App.jsx
+++ React/client/src/App.jsx
@@ -5,11 +5,16 @@ import C from "./Components/C";
 // import A from "./Parent to child/A";
 import AA from "./Child to Child/AA";
 import Child from "./State/Parent to child/Child";
-import Parent1 from "./State/Child to Parent/Parent1";
+// import Parent1 from "./State/Child to Parent/Parent1";
 // import Parent from "./State/Child to child/Parent";
 import P from "./State/29-01-2026/P";
-import A from "./State/29-01-2026/EX -1/A"
-import Parent from "./State/29-01-2026/EX-2/Parent";
+// import A from "./State/29-01-2026/EX -1/A"
+// import Parent from "./State/29-01-2026/EX-2/Parent";
+import A from "./State/30-01-2026/Props/A"
+import Parent from "./State/30-01-2026/Props drilling/Parent"
+import Parent1 from "./State/30-01-2026/State lifiting/Parent1"
+
+
 
 function App(){
 
@@ -17,15 +22,19 @@ function App(){
 
   <>
       
-      {/* <B/>
+      <B/>
       <C/>
       <AA/>
       <Child/>
       <Parent1/>
-      <Parent/>  */}
+      <Parent/> 
       <P/> 
       <A/>
       <Parent/>
+      <A/>
+      
+      
+      
    </>
 
   )
